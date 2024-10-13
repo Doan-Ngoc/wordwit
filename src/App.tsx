9dfb78b682e566eff6f4120103c47f1776452e51
Minh Ngoc Doan <minhngocdoan3112@gmail.com>
2024-10-13 16:26:42 +0700

    Board, Square, Keyboard, Key

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Heading from './components/Heading/Heading';
+import Board from './components/Board/Board';
+import Keyboard from './components/Keyboard/Keyboard';
 
 function App() {
+  const [board, setBoard] = useState<string[]>(
+    ["", "", "", "", "",
+     "", "", "", "", "",
+     "", "", "", "", "",
+     "", "", "", "", "",
+     "", "", "", "", "",
+     "", "", "", "", ""
+    ]
+  )
   return (
     <div className="App">
       <Heading type="h1" text="Wordwit" />
       <Heading type="subtitle" text="Another Wordle clone" />
+      <div className='board-container'>
+        <Board board={board}/>
+      </div>
+      <Keyboard />
     </div>
   );
 }
